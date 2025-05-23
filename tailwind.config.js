652bf61a89e34b0ba1eeb6e7afce58f0b367be98
Phuc Le <phucle@Phucs-MacBook-Air.local>
2025-05-23 16:41:14 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // quét class Tailwind trong src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
