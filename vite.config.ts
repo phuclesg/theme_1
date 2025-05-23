845181a0041a91ebeeaf3b889a62b0adec3ef8a6
Phuc Le <phucle@Phucs-MacBook-Air.local>
2025-05-23 17:16:12 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base:'/theme_1-1/',
+  base:'/theme_1/',
   plugins: [
     tailwindcss(),
   ],
